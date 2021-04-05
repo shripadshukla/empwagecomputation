7f5a3485c0f5e0b18091c707ca117451a49ebd9a
shripadshukla <shripadshukla46@gmail.com>
2021-04-05 10:31:39 +0530

    add: UC2 Calculate daily emp wage

--- empcheck.sh
+++ empcheck.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
 isPresent=1
 randomCheck=$((RANDOM%2))
+totalworkingHrs=20
+empHrs=8
 if [ $isPresent -eq $randomCheck ]
 then
 	echo"emp is present"
+	sal=$((totalworkingHrs*empHrs))
 else
 	echo "emp Absent"
+	sal=0
 done
